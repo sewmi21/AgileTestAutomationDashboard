d34fdce3111a40024c7e859d69444aa3c42c89e2
sewmi_M <sewmi.m@synergenhealth.com>
2021-09-28 20:56:05 +0530

    Initial Commit

---
+++ src/main/java/tech/sewmi/TestAutomationDashboard/TestAutomationDashboardApplication.java
@@ -0,0 +1,13 @@
+package tech.sewmi.TestAutomationDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAutomationDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAutomationDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/sewmi/TestAutomationDashboard/TestAutomationDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sewmi.TestAutomationDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAutomationDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
